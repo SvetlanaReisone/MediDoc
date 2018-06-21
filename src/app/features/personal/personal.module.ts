b85941a02cf5d2f2019e470e0b0b6904b017bc71
Svetlana Reisone <sveta_r@bluewin.ch>
2018-06-21 13:05:24 +0200

    Commit personal material

--- src/app/features/personal/personal.module.ts
+++ src/app/features/personal/personal.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PersonalRoutingModule } from './personal-routing.module';
 import { PersonalPageComponent } from './containers/personal-page/personal-page.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
     PersonalRoutingModule
   ],
-  declarations: [PersonalPageComponent]
+  declarations: [PersonalPageComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PersonalModule { }
